aab851b94d97c8f31c6bc1628ab532e167510134
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-01-16 22:46:19 +0530

    restructure routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,30 @@
 //* main react imports 
 import React from 'react'
 
-//* MUI components imports 
-import { ThemeProvider } from "@emotion/react"
-
-//* native theme imports 
-import theme from './theme'
-
 //* native components/sections imports 
 import ScrollTop from "./components/ScrollTop"
 import Footer from './sections/Footer'
+import Navbar from './sections/Navbar'
 
 //* pages imports 
 import Dashboard from './pages/Dashboard'
 import GrievanceForm from './pages/GrievanceForm'
 
 //* react router imports 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './sections/Navbar'
+import { Routes, Route } from 'react-router-dom'
+
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <React.Fragment>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/form" element={<GrievanceForm />} />
-          </Routes>
-          <ScrollTop />
-          <Footer />
-        </Router>
-      </React.Fragment>
-    </ThemeProvider>
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/form" element={<GrievanceForm />} />
+      </Routes>
+      <ScrollTop />
+      <Footer />
+    </React.Fragment>
+
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+
+//* native theme imports 
+import theme from './theme'
+
+//* router dom imports 
+import { BrowserRouter } from 'react-router-dom';
+
+//* MUI components imports 
+import { ThemeProvider } from "@emotion/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
