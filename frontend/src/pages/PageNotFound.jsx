5081e2bf9fd79d7ecb637bbca2e0c7b4144b799b
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-03-13 12:19:24 +0530

    Delete frontend directory

--- frontend/src/pages/PageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export default function PageNotFound() {
-  return (
-    <Box sx={{ marginTop: 12, marginBottom: 2, textAlign: "center" }}>
-      Page Not Found
-    </Box>
-  );
-}
